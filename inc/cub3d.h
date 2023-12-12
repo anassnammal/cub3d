2020adc0c8fac52d4555e3cff8967bca314c1a61
Anass Nammal <anammal@e3r10p14.1337.ma>
2023-12-12 05:36:26 +0100

    done

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -5,6 +5,14 @@
 # include <stdio.h>
 # include <fcntl.h>
 
-
+typedef struct s_scene
+{
+	void	*north;
+	char	*south;
+	char	*west;
+	char	*east;
+	int 	floor;
+	int 	ceiling;
+}	t_scene;
 
 #endif
